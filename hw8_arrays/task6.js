8a27220912e5ad77a38713be2c2e0932da40d9dd
Oleksii Lysovenko <oleksii.lysovenko@travisperkins.co.uk>
2024-05-16 12:14:34 +0300

    fix of sorting numbers

--- hw8_arrays/task6.js
+++ hw8_arrays/task6.js
@@ -1,5 +1,14 @@
-let initArray = [1,10,14,2,4,5,43,34];
+/*let initArray = [1,10,14,2,4,5,43,34];
 let sortedArray = initArray.sort((a , b) => a - b);
 
 console.log(sortedArray);
-console.log(initArray);
+console.log(initArray);*/
+
+
+
+let initArray1 = [1,10,14,2,4,5,43,34];
+let sortedArray1 = [...initArray1];
+sortedArray1.sort((a, b) => a - b);
+
+console.log(sortedArray1);
+console.log(initArray1)
