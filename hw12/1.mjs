04c84088ee18972e42bd8f0d8b5eafdabe6b738d
Oleksii Lysovenko <oleksii.lysovenko@travisperkins.co.uk>
2024-06-08 01:09:22 +0300

    prettier, eslint, husky configs

---
+++ hw12/1.mjs
@@ -0,0 +1,18 @@
+console.log("Helooooo");
+
+const object = {name: "Alice"};
+console.log(object);
+
+let string = {nice: "Derrick"};
+console.log(string);
+
+if (1) {
+    console.log("hi");
+}
+
+let obj = {
+    name: "This name is tooo loooooooong for testing of print width prettier of ",
+    age: 1278271
+};
+
+console.log(obj);
